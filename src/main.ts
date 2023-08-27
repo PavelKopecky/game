c5e9faf38f4c43fa238c40e52d7d15a85e608a06
Pavel Kopecký <kopecp14@fit.cvut.cz>
2023-08-27 11:53:07 +0200

    cool stuff

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ class obstacle {
         const img = document.createElement('img');
         e.classList.add('game-object');
 
-        for (let i = 0; i < 3; ++i) {
+        for (let i = 0; i < 5; ++i) {
             if (currentGame.availableObstacleId[i]) {
                 currentGame.availableObstacleId[i] = false;
                 this.id = i;
@@ -18,31 +18,31 @@ class obstacle {
         switch (this.randomType()) {
 
             case 1 : {
-                this.height = 70;
-                this.width = 70;
+                this.height = 72;
+                this.width = 72;
                 e.classList.add('obstacle-type1');
-                img.src = '/src/img/obstacle1.png';
+                img.src = obstacleType1.src;
                 break;
             }
             case 2 : {
-                this.height = 80;
-                this.width = 100;
+                this.height = 96;
+                this.width = 96;
                 e.classList.add('obstacle-type2');
-                img.src = '/src/img/obstacle2.png';
+                img.src = obstacleType2.src;
                 break;
             }
             case 3 : {
-                this.height = 90;
-                this.width = 140;
+                this.height = 128;
+                this.width = 128;
                 e.classList.add('obstacle-type3');
-                img.src = '/src/img/obstacle3.png';
+                img.src = obstacleType3.src;
                 break;
             }
             default : {
                 this.height = 70;
                 this.width = 70;
                 e.classList.add('obstacle-type1');
-                img.src = '/src/img/obstacle1.png';
+                img.src = obstacleType1.src;
                 break;
             }
         }
@@ -52,7 +52,8 @@ class obstacle {
     }
 
     randomType = () : number => {
-        return currentGame.difficulties[currentGame.difficulty - 1][Math.floor(Math.random() * 10) % 5];
+        if (currentGame.difficulty >= 7) return 3;
+        else return currentGame.difficulties[(currentGame.difficulty - 1)][Math.floor(Math.random() * 10) % 5];
     }
 
     startMovement = () => {
@@ -66,7 +67,10 @@ class obstacle {
                 currentGame.obstacles.shift();
             }
         }, 2000);
-        if (!currentGame.gameRunning) currentGame.interval();
+        if (!currentGame.gameRunning) {
+            document.querySelector('.ground')!.classList.add('ground-move');
+            currentGame.interval();
+        }
     }
 
     x : number = 1800;
@@ -86,10 +90,11 @@ class game {
     gameRunning : boolean = false;
     score : number = 0;
     frequency : number = 1500;
-    availableObstacleId : [boolean, boolean, boolean] = [true, true, true];
-    difficulties = [[1,1,1,1,2],[1,1,2,2,2],[1,2,2,2,3],[1,2,2,3,3],[2,2,3,3,3]];
+    availableObstacleId : [boolean, boolean, boolean, boolean, boolean] = [true, true, true, true, true];
+    difficulties = [[1,1,1,1,2],[1,1,2,2,2],[1,2,2,2,3],[1,2,2,3,3],[2,2,3,3,3],[2,3,3,3,3]];
     highScore : number = 0;
     jmpCount : number = 0;
+    groundInterval : number = 0;
 
     startGame = () => {
         let begin = document.querySelector('.begin');
@@ -118,11 +123,19 @@ class game {
         s.classList.add('score','game-object');
         s.innerHTML = '0';
         document.body.appendChild(s);
-        const e = document.createElement('p');
+        const e = document.createElement('div');
         e.classList.add('ground','game-object');
+        const img1 = document.createElement('img');
+        img1.src = '/src/img/ground.png';
+        img1.style.left = '0';
+        e.classList.add('ground','game-object');
+        const img2 = document.createElement('img');
+        img2.src = '/src/img/ground.png';
+        img2.style.left = '2560';
+        e.appendChild(img1);
+        e.appendChild(img2);
         document.body.appendChild(e);
         setTimeout(() => {
-            document.querySelector('.ground')!.classList.add('ground-move');
             document.querySelector('.body')!.classList.add('bck-change');
             document.querySelector('.score')!.classList.add('score-appear');
         }, 100);
@@ -131,6 +144,7 @@ class game {
     addPlayer = () => {
         const e = document.createElement('div');
         const img = document.createElement('img');
+        img.classList.add('player-img');
         img.src = '/src/img/player1.png'
         e.classList.add('player','game-object');
         e.appendChild(img);
@@ -166,13 +180,13 @@ class game {
         const gameInt = setInterval(() => {
             this.obstacles.forEach((obstacle) => {
                 obstacle.x -= 10;
-                if (obstacle.x <= 180 + obstacle.width/2 && obstacle.x >= 180 - obstacle.width/2 && !this.playerUp) {
+                if (obstacle.x <= 180 + obstacle.width/2 + 18 && obstacle.x >= 180 - obstacle.width/2 - 18 && !this.playerUp) {
                     clearInterval(gameInt);
                     this.gameLost();
                 } else if (obstacle.x === 100) {
                     this.score++;
                     document.querySelector('.score')!.innerHTML = String(this.score);
-                    if (this.score % 10 === 0 && this.difficulty < 5) this.difficulty++;
+                    if (this.score % 10 === 0 && this.difficulty < 10) this.difficulty++;
                 }
             });
         }, 10);
@@ -180,10 +194,11 @@ class game {
 
     playerJump = () => {
         if (!this.gameRunning) return;
-        const player = document.querySelector('.player') as HTMLElement;
-        player.style.rotate = `${(++this.jmpCount * 90)}deg`;
+        const player = document.querySelector('.player');
         if (player!.classList.contains('player-jump')) return;
         else {
+            const playerImg = document.querySelector('.player-img') as HTMLElement;
+            playerImg.style.rotate = `${(++this.jmpCount * 90)}deg`;
             player!.classList.add('player-jump');
             setTimeout( () => {
                 this.playerUp = true;
@@ -207,13 +222,15 @@ class game {
             localStorage.setItem('highscore', String(this.highScore));
         }
 
-        for (let i = 0; i < 3; ++i) {
+        for (let i = 0; i < 5; ++i) {
             if (!this.availableObstacleId[i]) {
                 document.querySelector(`.obstacle${i}`)!.classList.add('paused');
             }
         }
 
         document.querySelector('.player')!.classList.add('paused');
+        document.querySelector('.ground')!.classList.add('paused');
+        clearInterval(this.groundInterval);
 
         setTimeout(() => {
             //end screen
@@ -266,17 +283,25 @@ class game {
         document.querySelector('.end-screen')!.remove();
         document.querySelector('.body')!.classList.remove('bck-change');
         this.difficulty = 1;
-        this.availableObstacleId = [true, true, true];
+        this.availableObstacleId = [true, true, true, true, true];
         this.obstacles = [];
         this.jumpHeight = 500;
         this.gameOver = false;
         this.playerUp = false;
         this.gameRunning = false;
         this.score = 0;
+        this.jmpCount = 0;
         this.startGame();
     }
 }
 
 let currentGame = new game();
+
+let obstacleType1 = new Image();
+obstacleType1.src = '/src/img/obstacle1-1.png';
+let obstacleType2 = new Image();
+obstacleType2.src = '/src/img/obstacle2-1.png';
+let obstacleType3 = new Image();
+obstacleType3.src = '/src/img/obstacle3-1.png';
 
 document.querySelector('.begin-btn')!.addEventListener('click', currentGame.startGame);
